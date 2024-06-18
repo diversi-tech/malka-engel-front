3685479c0aad6b877840265f45285866d09e3d24
Batsheva <Btsh4056@gmail.com>
2024-06-18 12:17:59 +0300

    הגדרת אופציה של שינוי שפה

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslation from './components/i18n/en.json';
+import heTranslation from './components/i18n/he.json';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +16,29 @@ root.render(
   </React.StrictMode>
 );
 
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      he: { translation: heTranslation }
+    },
+    lng: 'he', // // שפת ברירת המחדל
+    fallbackLng: 'he', // שפת ניפוי אם אין תרגום שנמצא
+    interpolation: {
+      escapeValue: false // React כבר יוצא מצב יציאה
+    }
+  });
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+export default i18n;
+
+
 reportWebVitals();
+
