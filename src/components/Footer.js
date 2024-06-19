81fa1ebc1fb0539f9b63a8fd8bd9b4bb0be2d2dc
Batsheva <Btsh4056@gmail.com>
2024-06-19 15:14:22 +0300

    chenges

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, Outlet } from "react-router-dom"
-import React from 'react';
+// import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 //Footer page
