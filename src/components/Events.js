a37be5c89f7397a28b4753fd6bab7e6d5cf8f55f
YMendelzon <159305317+YMendelzon@users.noreply.github.com>
2024-06-19 12:45:25 +0300

    the whole footer

---
+++ src/components/Events.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+//Home page
+export const Events = () => {
+    const { t, i18n } = useTranslation();
+    return (
+       <div>
+        <h1>{t('eventsPage.title')}</h1>
+        
+       </div>
+    );
+}
--- src/components/MyRouting.js
+++ src/components/MyRouting.js
@@ -5,7 +5,9 @@ import { Footer } from "./Footer";
 import { MyAccount } from "./MyAccount";
 import { CommonQuestions } from "./CommonQuestions";
 import { Contact} from "./Contact";
-
+import {Joys} from "./Joys";
+import {DesignerBirkins} from "./DesignerBirkins";
+import {CongratulationsToTheDonors} from "./CongratulationsToTheDonors";
 
 export const MyRouting = () => {
     return (
@@ -16,7 +18,9 @@ export const MyRouting = () => {
                 <Route path="/Contact" element={<Contact></Contact>}></Route>
                 <Route path="/CommonQuestions" element={<CommonQuestions></CommonQuestions>}></Route>
                 <Route path="/MyAccount" element={<MyAccount></MyAccount>}></Route>
-
+                <Route path="/Joys" element={<Joys></Joys>}></Route>
+                <Route path="/designerBirkins" element={<DesignerBirkins></DesignerBirkins>}></Route>
+                <Route path="/CongratulationsToTheDonors" element={<CongratulationsToTheDonors></CongratulationsToTheDonors>}></Route>
             </Routes>
             <Footer></Footer>
                 
