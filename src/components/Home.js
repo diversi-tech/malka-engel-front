6f74ea140aa6664d4c55d8473737b506c47cd8fc
YMendelzon <159305317+YMendelzon@users.noreply.github.com>
2024-06-20 16:11:08 +0300

    recommendedProducts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import { useTranslation } from 'react-i18next';
+import { RecommendedProducts } from './RecommendedProducts';
 //Home page
 export const Home = () => {
     const { t, i18n } = useTranslation();
     return (
        <div>
         <h1>{t('homePage.title')}</h1>
-        
+        <RecommendedProducts></RecommendedProducts>
        </div>
     );
 }
