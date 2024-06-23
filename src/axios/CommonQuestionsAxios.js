60ace06c5a867e2e71c9d021cdc9265e8647f9d2
Sara Sofer <sr6737543@gmail.com>
2024-06-23 15:48:08 +0300

    Edit on FQA page & edit on Axios

---
+++ src/axios/CommonQuestionsAxios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_BASE_URL = process.env.REACT_APP_API_URL+"/api/CommonQuestions/"
+
+export const getCommonQuestions=async(language)=>{
+  debugger
+try{
+let result = await axios.get(`${url}GetAllCommonQuestions?Language=${language}`)
+return result.data
+}
+catch(ch){
+  console.log(ch)
+}
+}
+
