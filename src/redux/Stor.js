8890762ea3140a4aa6e1eaced238bdc150496869
Tami Vahab <tm80009000@gmail.com>
2024-06-24 17:36:48 +0300

    create the infrastructure for Reviews

--- src/redux/Stor.js
+++ src/redux/Stor.js
@@ -4,7 +4,8 @@ import { DataReducer_FQA } from "./DateReducer/dataReducer.FQA";
 
 const reducers = combineReducers(
     { 
-    DataReducer_FQA: DataReducer_FQA
+    DataReducer_FQA: DataReducer_FQA,
+    
 }
 )
 export const Store = createStore(reducers)
