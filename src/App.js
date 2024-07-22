c92a1599502c24f8c4d8a272b42556819dc86d23
Ayala E <AYALA76745@GMAIL.com>
2024-07-22 09:12:44 +0300

    ayala and esti aws

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyRouting from './components/Layout Components/MyRouting.js';
 import Home from './components/Layout Components/Home.js';
 
 import { Subscriber } from 'rxjs';
+import FileUpload from './components/FileUpload.js';
 
 function App() { 
 
@@ -14,6 +15,8 @@ function App() {
     <div className="App">
       <Provider store={Store}>
         <MyRouting></MyRouting>
+        {/* <FileUpload></FileUpload> */}
+        {/* <img src='https://designery.s3.eu-north-1.amazonaws.com/A.jpg'></img> */}
         {/* <RecommendedProducts></RecommendedProducts> */}
       </Provider>
     </div> 
