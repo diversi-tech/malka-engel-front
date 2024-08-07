73843de446b95d38f29c8be2f3789bb7615646d9
Tami Vahab <tm80009000@gmail.com>
2024-08-08 01:03:16 +0300

    BH commit

--- src/HomePageFigma.js
+++ src/HomePageFigma.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BuilderComponent, builder } from '@builder.io/react';
 
 // החלף את 'YOUR_API_KEY' במפתח ה-API שלך
-builder.init('ef9427277e264c8689653fe2edea2fbc');
+builder.init('bpk-b574c3baf5dd4330854176487c8ae71b');
 
 // צור את הקומפוננטה שלך
 const Page = ({ content }) => {
---
+++ src/components/TYH_EOM/THY.js
@@ -0,0 +1,49 @@
+
+
+
+export const TYH = () => {
+    return (
+        <div style={{width: 1920, height: 1080, position: 'relative', background: 'white'}}>
+        <div className="Vector11" style={{width: 0, height: 1126.49, left: 125, top: -47, position: 'absolute', border: '1px #1E1F2F solid'}}></div>
+        <div className="Line5" style={{width: 1972, height: 0, left: 1971, top: 109, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0', border: '1px #1E1F2F solid'}}></div>
+        <div className="Vector15" style={{width: 1783, height: 0, left: -5, top: 1001, position: 'absolute', border: '1px #1E1F2F solid'}}></div>
+        <div className="CreatedByRinatChen2024" style={{width: 283.45, height: 31.71, left: 744, top: 1028, position: 'absolute', textAlign: 'right', color: '#1E1F2F', fontSize: 20, fontFamily: 'Alef', fontWeight: '400', lineHeight: 27, wordWrap: 'break-word'}}> Created by Rinat chen  © 2024</div>
+        <div className="Frame163" style={{height: 151, left: 210, top: 0, position: 'absolute', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+          <div className="Frame25" style={{height: 151, justifyContent: 'flex-start', alignItems: 'center', gap: 296, display: 'inline-flex'}}>
+            <div className="Frame1" style={{height: 53, justifyContent: 'center', alignItems: 'center', gap: 199, display: 'flex'}}>
+              <div style={{textAlign: 'right', color: '#1E1F2F', fontSize: 36, fontFamily: 'Heebo', fontWeight: '400', wordWrap: 'break-word'}}>צור קשר</div>
+              <div style={{textAlign: 'right', color: '#1E1F2F', fontSize: 36, fontFamily: 'Heebo', fontWeight: '400', wordWrap: 'break-word'}}>המלצות</div>
+              <div style={{textAlign: 'right', color: '#1E1F2F', fontSize: 36, fontFamily: 'Heebo', fontWeight: '400', wordWrap: 'break-word'}}>מוצרים</div>
+              <div style={{textAlign: 'right', color: '#1E1F2F', fontSize: 36, fontFamily: 'Heebo', fontWeight: '900', wordWrap: 'break-word'}}>מי אנחנו</div>
+              <div style={{textAlign: 'right', color: '#1E1F2F', fontSize: 36, fontFamily: 'Heebo', fontWeight: '400', wordWrap: 'break-word'}}>עמוד בית</div>
+            </div>
+            <div className="Frame21" style={{padding: 10, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+              <div className="Frame22" style={{padding: 10, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+                <div className="Line6" style={{width: 114, height: 0, transform: 'rotate(90deg)', transformOrigin: '0 0', border: '1px #1E1F2F solid'}}></div>
+              </div>
+            </div>
+            <div className="Frame22" style={{width: 20, padding: 10, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+              <div className="Line6" style={{width: 122, height: 0, transform: 'rotate(90deg)', transformOrigin: '0 0', border: '1px #1E1F2F solid'}}></div>
+            </div>
+            <div className="Frame178" style={{width: 174, height: 55, position: 'relative'}} />
+            <div className="Frame23" style={{padding: 10, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+              <div className="Frame22" style={{padding: 10, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+                <div className="Line6" style={{width: 115, height: 0, transform: 'rotate(90deg)', transformOrigin: '0 0', border: '1px #1E1F2F solid'}}></div>
+              </div>
+            </div>
+            <div className="Frame24" style={{width: 40, padding: 10, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+              <div className="Frame22" style={{width: 20, padding: 10, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'flex'}}>
+                <div className="Line6" style={{width: 114, height: 0, transform: 'rotate(90deg)', transformOrigin: '0 0', border: '1px #1E1F2F solid'}}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Frame177" style={{width: 160, height: 150, left: 1778, top: 926, position: 'absolute'}}>
+          <div style={{left: 33, top: 93, position: 'absolute', textAlign: 'center', color: '#1E1F2F', fontSize: 14, fontFamily: 'Fb Basis', fontWeight: '400', lineHeight: 17.02, wordWrap: 'break-word'}}>שלום כהן</div>
+          <div className="Icon" style={{width: 40.99, height: 46.95, left: 44, top: 28, position: 'absolute', border: '2px #1E1F2F solid'}}></div>
+          <div className="Line7" style={{width: 160, height: 0, left: 160, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', border: '1px #1E1F2F solid'}}></div>
+        </div>
+        <div className="Vector17" style={{width: 0, height: 1120, left: 1778, top: -36, position: 'absolute', border: '1px #1E1F2F solid'}}></div>
+      </div>
+    )
+}
